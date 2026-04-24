18b08fa6f3de2bb1dcebc2dccedad959de7ee7b3
faaizaan <fsdar3108@gmail.com>
2026-04-24 12:05:38 +0200

    creazione entities

---
+++ src/main/java/faizanshahzaddar/ultimoprogetto/entities/Utente.java
@@ -0,0 +1,111 @@
+package faizanshahzaddar.ultimoprogetto.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "utenti")
+public class Utente implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private String email;
+    @Column
+    @JsonIgnore
+    private String password;
+    @Column
+    private String nome;
+    @Column
+    private String cognome;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    protected Utente (){};
+
+    public Utente(String email, String password, String nome, String cognome, Role role){
+        this.email = email;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
