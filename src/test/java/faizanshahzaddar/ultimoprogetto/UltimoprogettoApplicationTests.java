63aa2cba8d3e72b7220b64e9e083eabf2b177856
faaizaan <fsdar3108@gmail.com>
2026-04-24 10:17:45 +0200

    Initial commit

---
+++ src/test/java/faizanshahzaddar/ultimoprogetto/UltimoprogettoApplicationTests.java
@@ -0,0 +1,13 @@
+package faizanshahzaddar.ultimoprogetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimoprogettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
